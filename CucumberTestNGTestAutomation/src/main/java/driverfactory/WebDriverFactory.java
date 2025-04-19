828d3b2814416ed6dad85339a018b98faf183d8a
smithheal <smith.r@eviden.com>
2025-04-19 22:01:01 +0530

    Update WebDriverFactory.java

--- CucumberTestNGTestAutomation/src/main/java/driverfactory/WebDriverFactory.java
+++ CucumberTestNGTestAutomation/src/main/java/driverfactory/WebDriverFactory.java
@@ -1,46 +1,24 @@
-package driverfactory;
+import org.openqa.selenium.edge.EdgeOptions;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+public class WebDriverFactory {
+    public static WebDriver setDriver() {
+        EdgeOptions options = new EdgeOptions();
 
-public final class WebDriverFactory {
+        // Headless for CI environment
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
 
-	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	
-	private WebDriverFactory() {
-		
-	}
+        // Generate unique user data dir
+        try {
+            Path tempProfile = Files.createTempDirectory("edge-profile");
+            options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public static void setDriver() {
-
-		String browser = System.getProperty("browser", "edge");
-		if (browser.equalsIgnoreCase("firefox")) {
-			driver.set(new FirefoxDriver());
-			getDriver().manage().window().maximize();
-			getDriver().manage().deleteAllCookies();
-		} else if (browser.equalsIgnoreCase("edge")) {
-			driver.set(new EdgeDriver());
-			getDriver().manage().window().maximize();
-			getDriver().manage().deleteAllCookies();
-		} else {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--remote-allow-origins=*");
-			driver.set(new ChromeDriver(options));
-			getDriver().manage().window().maximize();
-			getDriver().manage().deleteAllCookies();
-		}
-	}
-
-	public static WebDriver getDriver() {
-		return driver.get();
-	}
-
-	public static void closeDriver() {
-		driver.get().quit();
-		driver.remove();
-	}
-
+        return new EdgeDriver(options);
+    }
 }
